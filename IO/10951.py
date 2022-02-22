4a883151575fdfef6603c1ea37dcda7b11f929b2
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2022-02-22 23:21:09 +0900

    Update 10951.py

--- IO/10951.py
+++ IO/10951.py
@@ -1,6 +1,6 @@
 # 두 정수 a, b가 입력될때마다 두 수의 합을 출력
 
-# 참인 동안 계쏙 실행
+# 참인 동안 계속 실행
 while True :
   # a, b를 입력받으면 합을 출력
   try :
@@ -8,6 +8,6 @@ while True :
     
     print(a+b)
   
-  # 입력 값의 이상이 있을 경우 중단
-  except:
+  # 입력 값이 없을 경우 중단
+  except EOFError:
     break
