2f1210a13dcb684a86f342780797169ed8aa64a5
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2022-02-22 23:01:39 +0900

    Update 10950.py

--- IO/10950.py
+++ IO/10950.py
@@ -1,8 +1,13 @@
 # 입력받은 케이스 테스트 횟수만큼 두 정수 a, b의 값 더해 출력
 
-num = int(input()) # 케이스 테스트 횟수를 입력받아 num에 저장
+# 케이스 테스트 횟수를 입력받아 num에 저장
+num = int(input())
 
+# 입력받은 횟수만큼 a, b를 입력받아 합을 출력
 for i in range(num):
-  a, b = map(int, input().split())
-  
-  print(a+b)
+  try:
+    a, b = map(int, input().split())
+    print(a+b)
+
+  except:
+    break
