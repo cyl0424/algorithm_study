a8bd22a10e6ff1969e1ee76962aab26c4f27b077
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2022-02-24 23:58:49 +0900

    Update 10818.py

--- IO/10818.py
+++ IO/10818.py
@@ -2,8 +2,11 @@
 
 num = int(input())
 
-numList = list(map(int, input().split()))
-
+# list()를 이용해 입력받은 숫자들을 numList에 리스트 형태로 저장
+numList = list(map(int, input().split())
+               
+# sort()를 이용해 numList의 요소를 오름차순으로 정렬
 numList.sort()
 
+# 최솟값인 인덱스 0의 요소와, 최댓값인 인덱스 -1의 요소를 순서대로 출력
 print(numList[0], numList[-1])
