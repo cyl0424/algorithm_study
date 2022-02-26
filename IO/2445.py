c8763cba1f6cb724526ad560c08d2a5a2349aac9
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2022-02-26 23:03:55 +0900

    Create 2445.py

---
+++ IO/2445.py
@@ -0,0 +1,13 @@
+# 규칙 찾아서 별 찍기
+
+num = int(input())
+
+for i in range(1, num):
+    print('*' * i, end = '')
+    print(' ' * (2 * (num -i)), end = '')
+    print('*' * i)
+
+for j in range(num, 0, -1):
+    print('*' * j, end = '')
+    print(' ' * (2 * (num -j)), end = '')
+    print('*' * j)
