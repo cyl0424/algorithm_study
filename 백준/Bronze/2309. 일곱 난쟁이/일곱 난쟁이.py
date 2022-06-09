0314f60019f77dcd2fc030f95a5f25eb2d619fad
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2022-06-09 12:39:41 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,8 @@
+from itertools import combinations
+heights=[int(input()) for _ in range(9)]
+
+for i in combinations(heights, 7):
+    if sum(i) == 100:
+        for j in sorted(list(i)):
+            print(j)
+        break
