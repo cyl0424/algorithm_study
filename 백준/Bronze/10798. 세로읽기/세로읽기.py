69bb6523ef417f8eb10cb4249f30b201f6320a41
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2024-09-03 18:34:36 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -2,13 +2,12 @@ lists = []
 max_length = 0
 
 for _ in range(5):
-    new = list(input())
+    new = input()
     lists.append(new)
-
     if len(new) > max_length:
         max_length = len(new)
 
 for i in range(max_length):
     for l in lists:
-        if (len(l) >= i+1):
+        if i < len(l):
             print(l[i], end='')
