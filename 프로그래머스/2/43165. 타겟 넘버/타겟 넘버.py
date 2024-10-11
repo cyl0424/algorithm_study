189ee9e9cdf4f0bf1d43f6c78a135366ad14580e
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2024-10-11 16:34:13 +0900

    [level 2] Title: 타겟 넘버, Time: 183.33 ms, Memory: 49.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,15 @@
+# BFS
+def solution(numbers, target):
+    answer = 0
+    leaves = [0]
+    
+    for num in numbers:
+        tmp = []
+        for leave in leaves:
+            tmp.append(leave + num)
+            tmp.append(leave - num)
+        leaves = tmp
+    
+    answer = leaves.count(target)
+    
+    return answer
