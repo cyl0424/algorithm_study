af33620df2f5f2c40256bfca4736d3d59ade186a
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2024-10-14 01:02:21 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 90.76 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def solution(arr):
+    answer = [arr[0]]
+    
+    tmp = arr[0]
+    for i in range(1, len(arr)):
+        if tmp != arr[i]:
+            answer.append(arr[i])
+        tmp = arr[i]
+    return answer
