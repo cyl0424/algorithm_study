6458dbbd4f0ec3c23c2936c9234b50ad4ad24144
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2024-10-14 17:40:15 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,12 @@
+def solution(s):
+    if s.isdigit():
+        return int(s)
+    
+    answer = s
+    eng_num = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5,
+              'six':6, 'seven':7, 'eight':8, 'nine':9, 'zero':0}
+    
+    for k in eng_num.keys():
+        answer = answer.replace(k, str(eng_num[k]))
+
+    return int(answer)
