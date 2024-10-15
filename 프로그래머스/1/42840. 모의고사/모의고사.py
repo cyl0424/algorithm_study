2867f6ea80326c70034e560fdbe76b26f57d1143
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2024-10-15 16:54:14 +0900

    [level 1] Title: 모의고사, Time: 3.32 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -0,0 +1,29 @@
+def solution(answers):
+    answer = []
+    
+    st_1 = [1, 2, 3, 4, 5]
+    st_2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    st_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    s1 = 0
+    s2 = 0
+    s3 = 0
+    
+    for i in range(0, len(answers)):
+        if answers[i] == st_1[i%len(st_1)]:
+            s1 += 1
+        if answers[i] == st_2[i%len(st_2)]:
+            s2 += 1
+        if answers[i] == st_3[i%len(st_3)]:
+            s3 += 1
+    
+    max_score = max(s1, s2, s3)
+    
+    if s1 == max_score:
+        answer.append(1)
+    if s2 == max_score:
+        answer.append(2)
+    if s3 == max_score:
+        answer.append(3)
+        
+    return answer
