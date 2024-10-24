1c09eee5a3a08a7a1caeebdd4897bcafb36d901c
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2024-10-24 19:17:56 +0900

    [level 2] Title: H-Index, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,7 @@
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+    for i, v in enumerate(citations, start=1):
+        if v >= i:
+            answer = i
+    return answer
