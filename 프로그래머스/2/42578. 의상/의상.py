b50afb17fec766568a8b8f6638540f69dde243b1
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2024-10-24 19:18:22 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,16 +1,11 @@
 from collections import Counter
 
 def solution(clothes):
-    answer = 1
-    
     key_counts = Counter(key for _, key in clothes)
-    key_counts_dict = dict(key_counts)
+    print(key_counts)
     
-    cnt_list = key_counts_dict.values()
+    answer = 1
+    for count in key_counts.values():
+        answer *= count + 1
     
-    for i in cnt_list:
-        answer *= i + 1
-        
-    answer -= 1
-    
-    return answer
+    return answer - 1
