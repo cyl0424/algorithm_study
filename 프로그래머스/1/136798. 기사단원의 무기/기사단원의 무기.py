5cc8f27689937051e790a7b7ba0fedb56ce1e987
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2024-10-24 19:18:38 +0900

    [level 1] Title: 기사단원의 무기, Time: 940.12 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,24 +1,22 @@
 def solution(number, limit, power):
     answer = 0
-    nums = []
     
-    for n in range(1, number+1):
+    for n in range(1, number + 1):
         cnt = 0
         
         if n == 1:
-            nums.append(1)
+            cnt = 1
         else:
-            if n **0.5 == int(n**0.5):
-                cnt -= 1  
-                
-            for i in range(1, int(n**0.5)+1):
+            sqrt_n = int(n ** 0.5)
+            for i in range(1, sqrt_n + 1):
                 if n % i == 0:
-                    cnt += 2
-            
-            if cnt <= limit:
-                nums.append(cnt)
-            elif cnt > limit:
-                nums.append(power)
+                    cnt += 1
+                    if i != n // i:
+                        cnt += 1
+
+        if cnt > limit:
+            answer += power
+        else:
+            answer += cnt
     
-    answer = sum(nums)
     return answer
