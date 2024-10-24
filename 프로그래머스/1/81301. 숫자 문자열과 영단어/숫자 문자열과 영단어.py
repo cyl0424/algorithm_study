652d5c1e1a9f74a19e1ce5b6c2c76fe56f55ca8e
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2024-10-24 19:15:26 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -3,10 +3,10 @@ def solution(s):
         return int(s)
     
     answer = s
-    eng_num = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5,
-              'six':6, 'seven':7, 'eight':8, 'nine':9, 'zero':0}
+    eng_num = {'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5',
+              'six':'6', 'seven':'7', 'eight':'8', 'nine':'9', 'zero':'0'}
     
     for k in eng_num.keys():
-        answer = answer.replace(k, str(eng_num[k]))
+        answer = answer.replace(k, eng_num[k])
 
     return int(answer)
