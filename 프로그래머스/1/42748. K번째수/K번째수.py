bc6b383624f87a08eeb93e229c10c648bf27004a
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2025-02-02 21:11:12 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,6 +2,6 @@ def solution(array, commands):
     answer = []
     
     for c in commands:
-        tmp = sorted(array[c[0]-1:c[1]])
-        answer.append(tmp[c[2]-1])
+        tmp = sorted(array[c[0]-1:c[1]])[c[2]-1]
+        answer.append(tmp)
     return answer
