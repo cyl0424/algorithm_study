69936157849f69e954ed61117a8fce3b9656d91e
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2025-02-02 22:31:19 +0900

    [level 2] Title: 가장 큰 수, Time: 1309.90 ms, Memory: 21 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,6 +1,5 @@
 def solution(numbers):
-    numbers = sorted(numbers, key=lambda x: str(x)*3, reverse=True)
-    
+    numbers = sorted(numbers, key = lambda x: str(x)*3, reverse=True)
     answer = ''.join(map(str, numbers))
     
-    return str(int(answer))
+    return str(int(answer)) # 00일 경우 0으로 출력되도록 하기 위해 str -> int -> str로 출력
