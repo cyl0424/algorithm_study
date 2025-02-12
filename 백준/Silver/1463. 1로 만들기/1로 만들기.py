a3b2716bf55fd2537124bf156f5bcd1105aac6e5
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2025-02-12 09:19:01 +0900

    [Silver III] Title: 1로 만들기, Time: 468 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,18 +1,12 @@
 n = int(input())
 
-# DP 테이블 초기화
-dp = [0] * 1000001
+dp = [0] * (n+1)
 
-# 다이나믹 프로그래밍 진행(bottom-up)
 for i in range(2, n+1):
-    # 현재의 수에서 1을 빼는 경우
     dp[i] = dp[i-1] + 1
-    # 현재의 수가 2로 나누어 떨어지는 경우
-    if i%2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-    # 현재의 수가 3으로 나누어 떨어지는 경우
-    if i%3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
 
-# 결과 출력
 print(dp[n])
