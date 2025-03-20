5f6c49f5c99b7838e7be16773d0338cb08746f87
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2025-03-20 14:54:27 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.05 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,15 +1,8 @@
+import java.io.*;
+
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        char[] char_list = my_string.toCharArray();
-        
-        char[] reverse = new char[char_list.length];
-        
-        for (int i = char_list.length; i > 0; i--){
-            reverse[i-1] = char_list[char_list.length - i];
-        }
-        
-        answer = new String(reverse);
+        String answer = new StringBuilder(my_string).reverse().toString();
         return answer;
     }
 }
