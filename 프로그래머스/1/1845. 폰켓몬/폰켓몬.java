0af775cde4873538708c621e624a32ca3d9fe09a
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2025-03-20 17:56:01 +0900

    [level 1] Title: 폰켓몬, Time: 27.22 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
+        Set<Integer> set = new HashSet<>(
+            Arrays.stream(nums).boxed().collect(Collectors.toSet())
+        );
+        int answer = set.size();
+        if (answer > nums.length/2){
+            answer = nums.length/2;
         }
-        return Math.min(set.size(), nums.length/2);
+        return answer;
     }
 }
