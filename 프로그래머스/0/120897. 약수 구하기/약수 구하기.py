8d9d739f041b8c09e5bae29e8908757499a02982
Chu Yulim <63573867+cyl0424@users.noreply.github.com>
2025-06-05 22:46:11 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 9.36 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,11 @@
+import math
+
+def solution(n):
+    answer = set()
+    for i in range(1, int(math.sqrt(n))+1):
+        if n % i == 0:
+            answer.add(i)
+            answer.add(n // i)
+        
+    answer = sorted(list(answer))
+    return answer
